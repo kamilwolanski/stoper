e196abda13144ebc75fd16909a85e221e3ece746
kamilwolanski <kwolanski3@gmail.com>
2020-08-20 20:31:13 +0200

    zmiana koloru i responsywnosc

--- main.js
+++ main.js
@@ -11,6 +11,18 @@ const timeList = document.querySelector('.time-list');
 
 const resetBtn = document.querySelector('.reset');
 
+const info = document.querySelector('.info');
+const closeBtn = document.querySelector('.close');
+
+const blueColor = document.querySelector('.blue');
+let root = document.documentElement;
+
+const orangeColor = document.querySelector('.orange');
+const greenColor = document.querySelector('.green');
+
+const brush = document.querySelector('.brush');
+
+const modalShadow = document.querySelector('.modal-shadow');
 
 let mseconds = 0;
 let seconds = 0;
@@ -46,14 +58,18 @@ const stopCount = ()=>{
 const lastTimeInfo = ()=>{
     clearInterval(count);
     
-    time.textContent = `Ostatni pomiar ${stopWatch.textContent}`;
     numberOfMeasurement++;
-    createLi(numberOfMeasurement, stopWatch.textContent);
+
+    if(stopWatch.textContent !== "0:00"){
+        createLi(numberOfMeasurement, stopWatch.textContent);
+        time.textContent = `Ostatni pomiar ${stopWatch.textContent}`;
+        time.style.visibility = "visible";
+    }
     stopWatch.textContent = "0:00";
+
     mseconds = 0;
     seconds = 0;
     click = 0;
-    time.style.visibility = "visible";
 
 }
 
@@ -94,8 +110,49 @@ const start = ()=>{
 }
 startBtn.addEventListener('click', start);
 
-// startBtn.addEventListener('click', ()=> count = setInterval(startCount, 10));
+const showInformation = ()=>{
+    document.querySelector('.modal-shadow').style.display = "block";
+    document.querySelector('.modal-shadow').classList.add("modal-animation");
+    
+}
+
+const closeInfo = ()=>{
+    document.querySelector('.modal-shadow').style.display = "none";
+}
+
+const changeColorToBlue = ()=>{
+    root.style.setProperty("--first-color", '#87cefa');
+    root.style.setProperty("--hover-color", '#58ace0');
+}
+
+const changeColorToOrange = ()=>{
+    root.style.setProperty("--first-color", '#FA1406');
+    root.style.setProperty("--hover-color", '#e2241a');
+
+}
+
+const changeColorToGreen = (e)=>{
+    root.style.setProperty("--first-color", '#32cd32');
+    root.style.setProperty("--hover-color", '#2db32d');
+}
+
+const showPanel = ()=>{
+    document.querySelector('.color-panel').classList.toggle('hidden');
+}
+
 pauzaBtn.addEventListener('click', stopCount);
 stopBtn.addEventListener('click', lastTimeInfo);
 archivesBtn.addEventListener('click', showMeasurement);
 resetBtn.addEventListener('click', cleanAll);
+info.addEventListener('click', showInformation);
+closeBtn.addEventListener('click', closeInfo);
+blueColor.addEventListener('click', changeColorToBlue);
+orangeColor.addEventListener('click', changeColorToOrange);
+greenColor.addEventListener('click', changeColorToGreen);
+brush.addEventListener('click', showPanel);
+window.addEventListener('click', e=> {
+    if(e.target === modalShadow){
+        document.querySelector('.modal-shadow').style.display = "none";
+    }
+})
+
